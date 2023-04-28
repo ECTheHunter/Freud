913f7db0c83e428e32bbc2f659cf956cbec65bad
canyilar <canylar2@gmail.com>
2023-04-28 23:43:24 +0300

    Rising Blood Added

---
+++ Assets/RisingBlood.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RisingBlood : MonoBehaviour
+{
+    [SerializeField] private float _raiseSpeed = 2.0f;
+    [field: SerializeField] public bool Stopped { get; set; } = false;
+
+    private void Update()
+    {
+        if (Stopped) return;
+
+        transform.localScale += _raiseSpeed * Time.deltaTime * Vector3.up;
+        transform.position += (_raiseSpeed * 0.5f) * Time.deltaTime * Vector3.up;
+    }
+}
